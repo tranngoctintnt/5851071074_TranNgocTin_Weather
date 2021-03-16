229d27819357a559cbe0080d97843025882ab6fc
NgocTin <ngoctin10a11@gmail.com>
2021-03-16 21:05:04 +0700

    Add project files.

---
+++ Weather/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Weather.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Weather/Models/WeatherInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Weather.Models
+{
+    [DataContract]
+    public class WeatherInfo
+    {
+        [DataMember(Name = "location")]
+        public string Location
+        {
+            get; set;
+        }
+
+        [DataMember(Name = "degree")]
+
+        public float Degree { get; set; }
+
+        [DataMember(Name = "dataTime")]
+
+        public DateTime DateTime
+        {
+            get; set;
+        }
+    }
+}
